f19da0d842078f193e1a52b32442b7c4d7b1b1cc
Jeet-Metapercept <jeetp@proximabiz.com>
2024-12-29 19:48:53 +0530

    langing

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,43 @@
 <script setup lang="ts">
-import packageJson from "@/package.json";
-
+defineProps<{ package: object }>();
+const config = useAppConfig();
 definePageMeta({
   title: "meta.home.title",
 });
 </script>
 
 <template>
-  <HomeWelcome :package="packageJson" />
+  <section class="container my-16 grid place-items-center">
+    <section class="grid text-center">
+      <h1 class="text-5xl font-bold mb-3">
+        {{ config.nuxtSiteConfig.name }}
+      </h1>
+      <p class="text-muted-foreground">
+        {{ config.nuxtSiteConfig.description }}
+      </p>
+      <div class="flex flex-wrap justify-center gap-3 my-10">
+        <Button as-child>
+          <NuxtLink target="_blank" to="https://nuxt.com">
+            <Icon name="logos:nuxt-icon" class="mr-2" />
+            Nuxt
+          </NuxtLink>
+        </Button>
+        <Button as-child>
+          <NuxtLink target="_blank" to="https://www.shadcn-vue.com">
+            <Icon name="simple-icons:shadcnui" class="mr-2 text-green-500" />
+            Shadcn
+          </NuxtLink>
+        </Button>
+        <Button as-child>
+          <NuxtLink
+            to="https://github.com/yusufalitangoz/nuxt-shadcn-boilerplate"
+            target="_blank"
+          >
+            <Icon name="simple-icons:github" class="mr-2" size="18" />
+            GitHub
+          </NuxtLink>
+        </Button>
+      </div>
+    </section>
+  </section>
 </template>
---
+++ pages/starter.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import packageJson from "@/package.json";
+
+definePageMeta({
+  title: "meta.home.title",
+});
+</script>
+
+<template>
+  <HomeWelcome :package="packageJson" />
+</template>
