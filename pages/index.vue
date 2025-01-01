3d7f76511e1e0134bafc85c75e2cc3fb0ed9030e
Jeet-Metapercept <jeetp@proximabiz.com>
2025-01-01 14:42:09 +0530

    ripple component

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts">
-const config = useAppConfig();
+// const config = useAppConfig();
 definePageMeta({
-  title: "meta.home.title",
+  title: "HoH Tracker",
 });
 </script>
 
 <template>
-  <section class="container mt-16">
-    <div class="text-center">
+  <section class="container mt-6">
+    <!-- <div class="text-center">
       <h1 class="text-5xl font-bold mb-3">
         {{ config.nuxtSiteConfig_name }}
       </h1>
       <p class="text-muted-foreground">
         {{ config.nuxtSiteConfig_description }}
       </p>
-    </div>
+    </div> -->
+
+    <!-- <div class="space-y-1 text-center">
+      <h4 class="text-xl font-medium leading-none">
+        {{ config.nuxtSiteConfig_name }}
+
+      </h4>
+      <p class="text-sm text-muted-foreground">
+        {{ config.nuxtSiteConfig_description }}
+      </p>
+    </div> -->
 
     <div class="flex flex-col justify-center gap-3 w-full">
       <HohStatus />
