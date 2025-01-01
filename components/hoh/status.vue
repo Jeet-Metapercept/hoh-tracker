20ca46c11bdf1ca0787d27112d1128d95b2bffd1
Jeet-Metapercept <jeetp@proximabiz.com>
2025-01-01 15:22:43 +0530

    Update status.vue

--- components/hoh/status.vue
+++ components/hoh/status.vue
@@ -42,6 +42,27 @@ const historyData = computed(() => {
   if (!historyDataRaw.value) return [];
   return [...historyDataRaw.value].reverse();
 });
+
+const steps = [
+  {
+    step: 1,
+    icon: 'lucide:user',
+    title: 'Your details',
+    description: 'Provide your name and email',
+  },
+  {
+    step: 2,
+    icon: 'lucide:salad',
+    title: 'Company details',
+    description: 'A few details about your company',
+  },
+  {
+    step: 3,
+    icon: 'lucide:vibrate-off',
+    title: 'Invite your team',
+    description: 'Start collaborating with your team',
+  },
+]
 </script>
 
 <template>
@@ -72,7 +93,7 @@ const historyData = computed(() => {
       </div>
 
       <!-- Status Controls -->
-      <div class="max-w-4xl flex flex-col justify-center mx-auto gap-4 mb-4">
+      <div class="max-w-4xl flex flex-col justify-center mx-auto gap-4">
         <Badge
           :variant="statusData?.status === 'True' ? 'default' : 'destructive'"
           class="justify-center"
@@ -81,22 +102,48 @@ const historyData = computed(() => {
           {{ statusData?.status === 'True' ? "Live" : "Offline" }}
         </Badge>
 
-        <div>
-          <Stepper>
-            <StepperItem :step="1">
-              <StepperTrigger>
-                <StepperIndicator>1</StepperIndicator>
-                <StepperTitle>Step 1</StepperTitle>
-                <StepperDescription>This is the first step</StepperDescription>
+        <div class="my-4">
+          <Stepper class="flex w-full items-start gap-2">
+            <StepperItem
+              v-for="step in steps"
+              :key="step.step"
+              v-slot="{ state }"
+              class="relative flex w-full flex-col items-center justify-center"
+              :step="step.step"
+            >
+              <StepperSeparator
+                v-if="step.step !== steps[steps.length - 1].step"
+                class="absolute left-[calc(50%+20px)] right-[calc(-50%+10px)] top-5 block h-0.5 shrink-0 rounded-full bg-muted group-data-[state=completed]:bg-primary"
+              />
+
+              <StepperTrigger as-child>
+                <Button
+                  :variant="state === 'completed' || state === 'active' ? 'default' : 'outline'"
+                  size="icon"
+                  class="z-10 rounded-full shrink-0"
+                  :class="[state === 'active' && 'ring-2 ring-ring ring-offset-2 ring-offset-background']"
+                >
+                  <Icon v-if="state === 'completed'" name="lucide:check"/>
+                  <Icon v-if="state === 'active'" name="svg-spinners:pulse"/>
+                  <Icon v-if="state === 'inactive'"  name="lucide:dot"/>
+                  
+                </Button>
               </StepperTrigger>
-              <StepperSeparator />
-            </StepperItem>
-            <StepperItem :step="2">
-              <StepperTrigger>
-                <StepperIndicator>2</StepperIndicator>
-                <StepperTitle>Step 2</StepperTitle>
-                <StepperDescription>This is the second step</StepperDescription>
-              </StepperTrigger>
+
+              <div class="mt-5 flex flex-col items-center text-center">
+                <StepperTitle
+                  :class="[state === 'active' && 'text-primary']"
+                  class="text-sm font-semibold transition lg:text-base"
+                >
+                  {{ step.title }}
+                </StepperTitle>
+                <StepperDescription
+                  :class="[state === 'active' && 'text-primary']"
+                  class="sr-only text-xs text-muted-foreground transition md:not-sr-only lg:text-sm"
+                >
+                  {{ step.description }}
+                </StepperDescription>
+              </div>
             </StepperItem>
           </Stepper>
         </div>
